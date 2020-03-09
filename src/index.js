bbda3e9eb1e2bdea9f5af52a084e66f3088d6309
David Behar <david@behar.io>
2020-03-09 01:07:42 -0600

    getEvenNumbers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const getEvenNumbers = (numbersArray) => {
-  // your code
+  return numbersArray.filter(number => number%2 === 0)
 }
+
+console.log(getEvenNumbers([1,2,3,4,5,6,8,9]))
 
 module.exports = getEvenNumbers;
