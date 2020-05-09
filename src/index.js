58b7e37ced3b65d21f2fd8df7628eab1dd0c9bb0
Razielini <razielco@gmail.com>
2020-05-08 20:15:32 -0600

    Update index.js
    Optimized to 1 line

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-const getEvenNumbers = (numbersArray) => {
-  return numbersArray.filter((a) => a%2 === 0)
-}
+const getEvenNumbers = (numbersArray) => numbersArray.filter((a) => a%2 === 0)
 
 module.exports = getEvenNumbers;
