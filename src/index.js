6df2c5c6dd57c86d76ce7c5d5ea508df9c2fc37f
Jorge Ossa <jorgehossa@gmail.com>
2020-05-23 08:47:57 -0500

    Challenge solved! 👌🏻

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const getEvenNumbers = (numbersArray) => {
-  // your code
-}
+const getEvenNumbers = (numbersArray) => numbersArray.filter(item => item % 2 === 0)
+  /* const evenArray = numbersArray.filter(item => item % 2 === 0);
+  return evenArray; */
 
 module.exports = getEvenNumbers;
