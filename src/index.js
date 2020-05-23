71d457098d96a5860139dd5ba85455f22b782850
Erickowski <daniel_perez_03081995@hotmail.com>
2020-05-23 17:50:03 -0500

    Change solved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const getEvenNumbers = (numbersArray) => {
-  // your code
+  const evenNumbers = numbersArray.filter(number => number % 2 == 0)
+  return evenNumbers;
 }
+
+getEvenNumbers([1,2,3,4,5,6,8,9]);
 
 module.exports = getEvenNumbers;
