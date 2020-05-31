1b95fc80bc9f9b67a95e37f2aba36cb213f8231f
Jedp <jedp082@gmail.com>
2020-05-31 14:46:07 -0500

    Challenge done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const getEvenNumbers = (numbersArray) => {
-  // your code
+const getEvenNumbers = (numbersArray) => {  
+  return numbersArray.filter( numb => numb%2 === 0);
 }
+
 
 module.exports = getEvenNumbers;
