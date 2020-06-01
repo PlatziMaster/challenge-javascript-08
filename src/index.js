cb83f283901d5d2c2b99dc5c68c6edddb4d678e5
miquel anqel <migueeldrg@gmail.com>
2020-06-01 14:10:16 -0500

    reto completado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+const numbersArray = [1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,18,19,20]
 const getEvenNumbers = (numbersArray) => {
-  // your code
+  const result = numbersArray.filter( n => n%2 === 0 )
+  return result
 }
 
 module.exports = getEvenNumbers;
