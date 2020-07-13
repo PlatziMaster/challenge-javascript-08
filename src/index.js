87e0741c6b1624d0e87b30717119bb9b68368ea3
Andres Valencia <cryos74@gmail.com>
2020-07-12 21:27:56 -0500

    Challenge completed 💪💪💪🏼

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const getEvenNumbers = (numbersArray) => {
-  // your code
+  const newArray = numbersArray.filter(number => number % 2 === 0)
+
+  return newArray
 }
 
 module.exports = getEvenNumbers;
