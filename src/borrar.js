cf191c81d310cc2b6ade284f30940bee88b9079c
Juan Daniel Rios Ramirez <mrdaniel19996@gmail.com>
2020-08-04 10:39:08 -0500

    [UPDATE] challenge done, filter even numbers

--- src/borrar.js
+++ src/borrar.js
@@ -1,7 +1,10 @@
+let array = [2,3,4,5,6,7,8,9];
+
 const getEvenNumbers = (numbersArray) => {  
   return answer = numbersArray.filter( x => x % 2 === 0)
 }
 
-module.exports = getEvenNumbers;
+getEvenNumbers(array);
+console.log(answer)
 
 
