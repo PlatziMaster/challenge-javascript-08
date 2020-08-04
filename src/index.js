d8528243c54d8041df88637eb0ada2d2f73bfe1b
Max23Esau <esa-max23@live.com>
2020-08-04 18:03:42 -0500

    add new .filter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const getEvenNumbers = (numbersArray) => {
 
-  const parNumber = numbersArray.filter(item => item % 2 == 0);
-  console.log(parNumber);
+  return numbersArray.filter(item => item % 2 === 0);
+
 }
 
-getEvenNumbers([1, 2, 4, 7, 10, 16, 19, 27, 34, 45, 53, 64]);
 
 module.exports = getEvenNumbers;
