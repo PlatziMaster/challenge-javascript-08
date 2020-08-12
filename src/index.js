490c65bd29b266a39ac46e662088a25cff842561
Johán Suárez <32972612+JohanSuarezQ@users.noreply.github.com>
2020-08-12 11:22:19 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 
-const pairArray = numbersArray.filter(function(numbers){
-    return numbers % 2 ===0
-})
-
-console.log(pairArray);
+ const getEvenNumbers = (numbersArray) => {
+    const pairArray = numbersArray.filter(function(numbers){
+        return numbers % 2 ===0
+    })
+    console.log(pairArray)
+}
+ 
 module.exports = getEvenNumbers;
