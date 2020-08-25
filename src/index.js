5692be3b3bfd91449f64ee2a7b38436b916a997e
juan sebastian herrera <juan.herreraca@campusucc.edu.co>
2020-08-25 15:03:20 -0500

    challenge finished

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-const getEvenNumbers = (numbersArray) => {
-  // your code
-}
 
+  let array = [2 ,3, 4, 5, 6]; 
+
+  const getEvenNumbers = (numbersArray) => {
+    return filteredNumbers = numbersArray.filter(   //filter crea un nuevo array con todos los elementos que 
+      numbers => numbers %2 <= 0 );                 // cumplan la condición implementada por la función dada.
+
+  }
+  
+  getEvenNumbers(array);
+  console.log(filteredNumbers)
+
+//
 module.exports = getEvenNumbers;
+
+
+// filteredNumbers = [2,4,6] 
