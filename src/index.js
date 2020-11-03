24d433b9493d65933aa998f9fe40eee5aa0e90a6
Daniel Saavedra <huyno50@gmail.com>
2020-11-02 19:24:16 -0500

    challenge-08-Resolved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+
 const getEvenNumbers = (numbersArray) => {
   // your code
+  const result = numbersArray.filter( number => number % 2 === 0 );
+  return result;
 }
 
 module.exports = getEvenNumbers;
+
+/** steps to make work:
+1) go to console in the navigator
+2) copy and paste the code
+3) write in the console: getEvenNumbers([put_you_numbers_here])
+4) result
