c4a66f4380a6c199de2fbca522ec1036e55fa897
Lidenbrock-ed <stivenh_1224@hotmail.com>
2021-05-24 17:23:25 -0500

    Solved challenge-javascript-08

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-const getEvenNumbers = (numbersArray) => {
-  // your code
+const getEvenNumbers = (numbersArray) => {  
+  return numbersArray.filter(number =>{
+    if(number % 2 == 0){
+      return number;
+    }
+  });
 }
 
 module.exports = getEvenNumbers;
